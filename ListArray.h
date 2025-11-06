722cd2fddb83321156283616a94a6887e7a86685
Oscar Molina Sirvent <omolsir@upv.edu.es>
2025-11-06 16:27:25 +0100

    Primera parte hecha

--- ListArray.h
+++ ListArray.h
@@ -10,23 +10,47 @@ class ListArray : public List<T>{
 	private:
 		T* arr;
 		int max;
-		int n = 0;
+		int n;
 		static const int MINSIZE = 2;
-		void resize(int new_size);
+		void resize(int new_size){
+			T* nuevo = new T[new_size];
+                        for(int i = 0; i < n; i++){
+                                nuevo[i] = arr[i];
+                        }
+                        delete[] arr;
+                        arr = nuevo;
+                        max = new_size;
+		}
+
 	public:
 		ListArray(){
 			arr = new T[MINSIZE];
 			max = MINSIZE;
-			n++;	
+			n = 0;	
 		}
 
 		~ListArray(){
-			delete arr;
+			delete[] arr;
 			arr = nullptr;
 		}
 
-		T operator[](int pos);
-		friend std::ostream& operator<<(std::ostream &out, const ListArray<T> &list);
+		T operator[](int pos){
+                	if(pos < 0 || pos >= n)
+				throw std::out_of_range("Posicion invalida en el operador");
+			else{
+				return arr[pos];
+			}
+       	 	}
+
+		friend std::ostream& operator<<(std::ostream &out, const ListArray<T> &list){
+			out << "List -> [" << std::endl;
+			for(int i = 0; i < list.n; i++ ){
+				out << list.arr[i] << std::endl;
+			}
+			out << "]" << std::endl;
+			return out;
+		}
+
 		void insert(int pos, T e) override{
 			if(pos < 0 || pos > n)
 				throw std::out_of_range("Posición invalida en insert");
@@ -52,30 +76,30 @@ class ListArray : public List<T>{
 			if(pos < 0 || pos > n)
 				throw std::out_of_range("Posicion invalida de remove");
 			ret = arr[pos];
-			for(int i = pos; i <= pos; i++){
+			for(int i = pos; i < n; i++){
 				arr[i] = arr[i + 1];	
 			}
+			n--;
 			return ret;
 		}
 
-		T get(int pos) override{
+		T get(int pos) const override{
 			return arr[pos];
 		}
 
-		int search(T e) override{
-			for(int i = 0; i > n; i++){
-				if(arr[i] = e)
+		int search(T e) const override{
+			for(int i = 0; i < n; i++){
+				if(arr[i] == e)
 					return i;
-				else
-					return -1;
 			}
+			return -1;
 		}
 
-		bool empty() override{
+		bool empty() const override{
 			return n == 0;
 		}
 
-		int size() override{
+		int size() const override{
 			return n;
 		}
 };
