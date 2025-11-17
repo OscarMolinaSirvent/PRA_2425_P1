6e3190c59948dd0707fc293a3ae34237446fd25c
Oscar Molina Sirvent <omolsir@upv.edu.es>
2025-11-17 16:27:56 +0100

    Point2D realizado

--- Parte2/src/Point2D.h
+++ Parte2/src/Point2D.h
@@ -7,25 +7,11 @@
 class Point2D{
 	public:
 		double x,y;
-		Point2D(double x=0, double y=0) : x{x}, y{y}{}
-		static double distance(const Point2D &a, const Point2D &b){
-			return (sqrt((pow(a.x,2) - pow(b.x,2) + 
-				(pow(a.y,2) - pow(b.y,2)))));
-		}
-
-		friend bool operator==(const Point2D &a, const Point2D &b){
-			return a.x == b.x && a.y == b.y;
-		}	
-
-		friend std::ostream& operator<<(std::ostream &out,const Point2D &p){
-			out << "X = " << p.x << "Y = " << p.y << std::endl;
-			return out;
-		}
-
-
-		friend bool operator!=(const Point2D &a, const Point2D &b){
-			return a.x != b.x && a.y != b.y;
-		}
+		Point2D(double x = 0, double y = 0);
+		static double distance(const Point2D &a, const Point2D &b);
+		friend bool operator==(const Point2D &a, const Point2D &b);
+		friend std::ostream& operator<<(std::ostream &out,const Point2D &p);
+		friend bool operator!=(const Point2D &a, const Point2D &p);
 };
 
 #endif
